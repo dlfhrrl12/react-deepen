06ab3666a0037926c441ae7fbac9c8d4d159cc21
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-02-19 22:06:52 +0900

    tanstack-query 2 기본 사용법(기초 todolist)

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,74 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import api from './axios/api';
+import { useState } from 'react';
 
 function App() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const queryClient = useQueryClient();
+  const [todoItem, setTodoItem] = useState('');
+  const fetchTodos = async () => {
+    const response = await axios.get('http://localhost:4000/todos');
+    return response.data;
+  };
+  const {
+    data: todos,
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: ['todos'],
+    queryFn: fetchTodos,
+  });
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const { data } = await api.get('/todos');
-        setData(data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchPost();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error : {error.message}</div>;
+  const addTodo = async (newTodo) => {
+    await axios.post('http://localhost:4000/todos', newTodo);
+  };
+  const { mutate } = useMutation({
+    mutationFn: addTodo,
+    onSuccess: () => {
+      alert('데이터 삽입 성공했습니다.');
+      queryClient.invalidateQueries(['todos']);
+    },
+  });
+  if (isPending) {
+    return <div>로딩중입니다...</div>;
+  }
+  if (isError) {
+    return <div>데이터 조회 중 오류가 발생했습니다.</div>;
+  }
 
   return (
     <div>
-      <h1>Fetched Data</h1>
+      <h3>TanStack Query</h3>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const newTodoObj = { title: todoItem, isDone: false };
+          mutate(newTodoObj);
+        }}
+      >
+        <input
+          type="text"
+          value={todoItem}
+          onChange={(e) => setTodoItem(e.target.value)}
+        />
+        <button>추가</button>
+      </form>
       <ul>
-        {data &&
-          data.map((item) => (
-            <li key={item.id}>
-              <h2>{item.title}</h2>
-              <p>{item.isDone ? 'Done' : 'Not Done'}</p>
+        {todos.map((todo) => {
+          return (
+            <li
+              key={todo.id}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '10px',
+                backgroundColor: 'aliceblue',
+              }}
+            >
+              <h4>{todo.title}</h4>
+              <p>{todo.isDone ? 'Done' : 'Not Done'}</p>
             </li>
-          ))}
+          );
+        })}
       </ul>
     </div>
   );
