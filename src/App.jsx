7032b4062780871a4327a3fb3e2ab2e34ab1ab6f
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-02-19 14:45:51 +0900

    비동기 1 - 비동기, promise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useEffect } from 'react';
+// import { useState } from 'react';
+
 function App() {
-  return <></>;
+  // const [message, setMessage] = useState('타이머 시작');
+
+  useEffect(() => {
+    /* const delay = function (ms) {
+       const promise = new Promise((resolve) => {
+         setTimeout(resolve, ms);
+       });
+       return promise;
+     };
+     delay(2000).then(() => {
+       setMessage('2초 후 메세지가 변경됐습니다.');
+   });*/
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
+
+  return <>{/* <h1>{message}</h1> */}</>;
 }
 
 export default App;
