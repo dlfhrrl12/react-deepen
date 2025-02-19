73535425200a2bf9c2625dd73c62c1e314a5c3f1
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-02-19 14:54:46 +0900

    비동기 2 - 비동기, promise.all

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { useEffect } from 'react';
-// import { useState } from 'react';
 
 function App() {
-  // const [message, setMessage] = useState('타이머 시작');
-
   useEffect(() => {
-    /* const delay = function (ms) {
-       const promise = new Promise((resolve) => {
-         setTimeout(resolve, ms);
-       });
-       return promise;
-     };
-     delay(2000).then(() => {
-       setMessage('2초 후 메세지가 변경됐습니다.');
-   });*/
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+    Promise.all([
+      fetch('https://jsonplaceholder.typicode.com/todos/1').then((response) =>
+        response.json()
+      ),
+      fetch('https://jsonplaceholder.typicode.com/todos/2').then((response) =>
+        response.json()
+      ),
+    ]).then(([response1, response2]) => {
+      console.log('response =>', response1);
+      console.log('response =>', response2);
+    });
   }, []);
-
-  return <>{/* <h1>{message}</h1> */}</>;
+  return (
+    <>
+      <div></div>
+    </>
+  );
 }
 
 export default App;
