f2cc50103b81a571c433d68878eadd7bebc22607
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-02-19 15:36:16 +0900

    json-server 패키지 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,5 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
-
 function App() {
-  const [post, setPost] = useState(null);
-
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const response = await fetch(
-          'https://jsonplaceholder.typicode.com/todos/1'
-        );
-        const data = await response.json();
-        setPost(data);
-      } catch (error) {
-        console.error('Error! => ', error);
-      }
-    };
-
-    fetchPost();
-  }, []);
-  console.log('post', post);
-  return (
-    <>
-      <h3>async / await 연습</h3>
-      {post ? <div>{post.title}</div> : <div>Loading...</div>}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
